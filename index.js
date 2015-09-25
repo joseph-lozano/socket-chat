eb06f6dea05236e1c57c0aa0722c5fd87e0f4e46
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-24 22:21:41 -0700

    port

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
+var port = process.env.PORT || 5000
+
+http.listen(port, function(){
+  console.log('listening on *:', port);
 });
 
