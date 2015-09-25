097e65affcbc9846be1abfe152004dba685781c8
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-25 10:16:43 -0700

    external css and script

---
+++ public/script.js
@@ -0,0 +1,34 @@
+var name = ""
+var socket = io();
+
+$('#name-input').submit(function(){
+  name = $('#n').val();
+  $('#n').val('');
+  $('#name-input').css('display', 'none')
+  $('#chat-box').css('display', 'block')
+  socket.emit('user joined', name);
+  return false
+})
+
+$('#chat-box').submit(function(){
+  msg = $('#m').val()
+  socket.emit('chat message', {message: msg, name: name});
+  $('#m').val('');
+  return false;
+});
+
+socket.on('chat message', function(msg){
+  $('#messages').append($('<li>').text(msg.name + ": " + msg.message));
+});
+
+socket.on('user joined', function(name){
+  $('#messages').append($('<li>').text(name + " has joined the chat!"))
+})
+
+// $('#m').on('input', function(){
+//   socket.emit('is typing', "user");
+// })
+// socket.on('is typing', function(usr){
+//   console.log('is typing')
+//   $('#messages').append($('<li>').text("User is Tpying"))
+// })
